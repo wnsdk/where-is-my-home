c3ff863d40848d72d8917576d0b5857110445999
wnsdk <jn307742@gmail.com>
2022-11-16 14:39:43 +0900

    Feat: QnA 기능 추가

---
+++ src/main/java/com/ssafy/qna/model/QnADto.java
@@ -0,0 +1,65 @@
+package com.ssafy.qna.model;
+
+public class QnADto {
+
+	private int articleNo;
+	private String userId;
+	private String userName;
+	private String subject;
+	private String content;
+	private String answer;
+	private int hit;
+	private String registerTime;
+	
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public int getArticleNo() {
+		return articleNo;
+	}
+	public void setArticleNo(int articleNo) {
+		this.articleNo = articleNo;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public String getRegisterTime() {
+		return registerTime;
+	}
+	public void setRegisterTime(String registerTime) {
+		this.registerTime = registerTime;
+	}
+
+}
