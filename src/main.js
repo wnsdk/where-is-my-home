04aab1d2cdc1334d5f93bfe0ffab571114010dfb
wnsdk <jn307742@gmail.com>
2022-11-17 16:18:40 +0900

    Feat: Argon 템플릿 적용

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Argon from "./plugins/argon-kit";
 
 import "@/api/lib/fontAwesomeIcon.js"; // fontAwesomeIcon.js 불러옴
 import "@/api/lib/vueBootstrap.js";
 
 Vue.config.productionTip = false;
-
+Vue.use(Argon);
 new Vue({
   router,
   store,
