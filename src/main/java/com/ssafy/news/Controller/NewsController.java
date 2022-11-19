d283030756fc74dba637672bd7b481c766275593
wnsdk <jn307742@gmail.com>
2022-11-19 12:33:20 +0900

    Fix: 뉴스 1페이지만 크롤링 해오는 이슈 해결

--- src/main/java/com/ssafy/news/Controller/NewsController.java
+++ src/main/java/com/ssafy/news/Controller/NewsController.java
@@ -1,7 +1,10 @@
 package com.ssafy.news.Controller;
 
 import java.io.IOException;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,30 +26,49 @@ public class NewsController {
  
     @RequestMapping(value="/crawl")
     public ResponseEntity<?> crawling() throws IOException {
-    	String url = "https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=101&sid2=260";
-        Document doc = Jsoup.connect(url).get();
-        Elements el = doc.select("ul.type06_headline");
+    	
+    	// 반환할 기사 리스트
+    	List<Map<String, String>> articles = new ArrayList<>();
+    	
+    	// 몇 페이지까지 뉴스기사가 있는지 구하기
+    	StringBuilder url = new StringBuilder("https://news.naver.com/main/list.naver?mode=LS2D&sid2=260&sid1=101&mid=shm");
+    	Document doc = Jsoup.connect(url.toString()).get();
+    	int lastpageno = Integer.parseInt(doc.select("div.paging>a:last-child").text());
+    	
+    	// 오늘 날짜 구하기
+    	Date today = new Date();
+    	SimpleDateFormat date = new SimpleDateFormat("yyyyMMdd");
+    	
+    	for (int pageno = 1; pageno < lastpageno + 1; pageno++) {
+    		url = new StringBuilder("https://news.naver.com/main/list.naver?mode=LS2D&sid2=260&sid1=101&mid=shm");
+        	url.append("&date=" + date.format(today));
+    		url.append("&page=" + pageno);
         
-        List<String> photos = el.select("li>dl>dt.photo>a>img").eachAttr("src");
-        List<String> links = el.select("li>dl>dt.photo>a").eachAttr("href");
-        List<String> subjects = el.select("li>dl>dt>a").eachText();
-        List<String> details = el.select("li>dl>dd>span.lede").eachText();
-        List<String> writers = el.select("li>dl>dd>span.writing").eachText();
-        List<String> dates = el.select("li>dl>dd>span.date").eachText();
-        
-        List<Map<String, String>> articles = new ArrayList<>();
-        
-        for (int i = 0; i < photos.size(); i++) {
-        	Map<String, String> article = new HashMap<>();
-        	
-        	article.put("photo", photos.get(i));
-        	article.put("link", links.get(i));
-        	article.put("subject", subjects.get(i));
-        	article.put("detail", details.get(i));
-        	article.put("writer", writers.get(i));
-        	article.put("date", dates.get(i));
-        	
-        	articles.add(article);
+			doc = Jsoup.connect(url.toString()).get();
+			
+			Elements el = doc.select("ul.type06_headline");
+            el.addAll(doc.select("ul.type06"));
+            if (pageno == 5) System.out.println(el);
+            
+            List<String> photos = el.select("li>dl>dt.photo>a>img").eachAttr("src");
+            List<String> links = el.select("li>dl>dt.photo>a").eachAttr("href");
+            List<String> subjects = el.select("li>dl>dt>a").eachText();
+            List<String> details = el.select("li>dl>dd>span.lede").eachText();
+            List<String> writers = el.select("li>dl>dd>span.writing").eachText();
+            List<String> dates = el.select("li>dl>dd>span.date").eachText();
+            
+            for (int i = 0; i < photos.size(); i++) {
+            	Map<String, String> article = new HashMap<>();
+            	
+            	article.put("photo", photos.get(i));
+            	article.put("link", links.get(i));
+            	article.put("subject", subjects.get(i));
+            	article.put("detail", details.get(i));
+            	article.put("writer", writers.get(i));
+            	article.put("date", dates.get(i));
+            	
+            	articles.add(article);
+    		}
 		}
         
         return new ResponseEntity<List<Map<String, String>>>(articles, HttpStatus.OK);
