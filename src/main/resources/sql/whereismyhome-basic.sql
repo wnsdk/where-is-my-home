b50b19dcef7e7e5acdae765e2bf9d6a98099c788
wnsdk <jn307742@gmail.com>
2022-11-23 17:44:26 +0900

    Fix: sql문 수정

--- src/main/resources/sql/whereismyhome-basic.sql
+++ src/main/resources/sql/whereismyhome-basic.sql
@@ -128,7 +128,7 @@ DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 delimiter $$
-CREATE PROCEDURE  insertDataQna()
+CREATE PROCEDURE insertDataQna()
 BEGIN
     DECLARE i INT default 1;
     WHILE (i <= 100) do
